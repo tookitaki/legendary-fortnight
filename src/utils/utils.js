213b5a8e7fb404ae94a847f2caff9355cd197764
Anish <Anish>
2021-05-18 06:48:07 +0530

    add lodash and simplify matchDispatchToProps

--- src/utils/utils.js
+++
@@ -1,5 +0,0 @@
-const isNotNull = (val) => val != null;
-const isNotUndefined = (val) => val !== undefined;
-
-export const isNotNullUndefined = (val) =>
-  [isNotNull, isNotUndefined].every((x) => x(val));
