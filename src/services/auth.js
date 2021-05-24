137e816d2119f30f6cf1938fb350580cd16ca8cb
Marcus Ang <marcus.ang@tookitaki.com>
2021-05-24 16:05:15 +0800

    Reverted services

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,20 +3,12 @@ import { API_URL } from '../constants/appConfig';
 
 export async function login({ username, password }) {
   const requestUrl = `${API_URL}/users/auth`;
-}
 
-export async function auth(token) {
-  const data = {
-    status: 200,
-    data: { token: 'erererreer', userId: 1, name: 'tdss_user' }
-  };
-  return data;
-}
+  const encodedCredentials = window.btoa(`${username}:${password}`);
 
-export async function logout(token) {
-  const data = {
-    status: 200,
-    data: null
+  const headers = {
+    Authorization: `Basic ${encodedCredentials}`
   };
-  return data;
+
+  return await axios.post(requestUrl, null, { headers });
 }
